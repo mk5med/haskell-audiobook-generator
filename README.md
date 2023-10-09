01ec9e05fbcb461e97a1601504bc51e7265c5490
local <8334048+mk5med@users.noreply.github.com>
2023-10-09 09:50:38 -0400

    Refactors and add splitting by line count
    - `substring :: Integer -> [String] -> ([String], [String])`
    - `splitByLineCount :: Integer -> [String] -> [[String]]`

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Easily transform books into audiobooks.
 
 Steps
 - [x] Accept books as txt files
-- [x] Split books by chapter and to max length of 1000 lines
+- [x] Split books by chapter
+- [x] Split chapters to a max length of 1000 lines
 - [ ] Use TTS model on each chapter
 - [ ] Zip all audio files
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,25 +1,11 @@
 module Lib
   ( processBook,
+    substring,
+    splitByLineCount,
   )
 where
 
-import Text.Regex.TDFA
-
--- Checks if a line is a chapter
-isChapter :: String -> Bool
-isChapter = (=~ "^Chapter +[0-9]+")
-
--- Checks if a line is a chapter and returns the line if it is
-chapterOrEmpty :: String -> String
-chapterOrEmpty inStr
-  | isChapter inStr = inStr
-  | otherwise = ""
-
--- Removes the chapter line from the first line if it exists
-removeChapter :: [String] -> [String]
-removeChapter inStrs
-  | isChapter (head inStrs) = tail inStrs
-  | otherwise = inStrs
+import Chapter (chapterOrEmpty, isChapter, removeChapter)
 
 -- Split a list of lines into sections separated by chapter
 extractSections :: [String] -> [[String]]
@@ -28,9 +14,39 @@ extractSections [] = [[""]]
 extractSections inStrs = do
   -- Remove the chapter if it exists
   let title = chapterOrEmpty (head inStrs)
-  let (chapter, next) = break isChapter (removeChapter inStrs)
+
+  -- Split the `inStrs` list into two
+  -- The first list contains all elements up to the first line that matches the pattern for chapter delimiters
+  -- break :: (a -> Bool) -> [a] -> ([a], [a])
+  -- Return: (in-order list of elements that do not match, next element that matches + everything else)
+  -- https://hackage.haskell.org/package/base-4.18.1.0/docs/Prelude.html#v:break
+  let (chapterContent, next) = break isChapter (removeChapter inStrs)
+
   -- Add the title back to the chapter text
-  (title : chapter) : extractSections next
+  (title : chapterContent) : extractSections next
+
+substring :: Integer -> [String] -> ([String], [String])
+-- Case: No available strings
+substring _ [] = ([], [])
+-- Case: Reached end of substring length
+substring 0 s = ([], s)
+substring i (cur : _rem) =
+  -- Keep taking elements until an end case is reached
+  let (y, ys) = substring (i - 1) _rem
+   in -- Combines thunks to create: ( cur1:(cur2:( ...:[] )), [] or remainer)
+      (cur : y, ys)
+
+splitByLineCount :: Integer -> [String] -> [[String]]
+splitByLineCount _ [] = []
+splitByLineCount l inStrs =
+  -- Get the substring
+  let (section, remainder) = substring l inStrs
+   in -- Append the substring to the front and process the remainder
+      -- Combines thunks to create: section1:(section2: ...:[] )
+      section : splitByLineCount l remainder
+
+addMarks :: [String] -> Integer -> [String]
+addMarks text indx = ["START OF CHAPTER " ++ show indx] ++ text ++ ["END OF CHAPTER " ++ show indx]
 
 processBook :: [String] -> [IO ()]
 processBook bookLines = do
@@ -43,16 +59,13 @@ processBook bookLines = do
   -- Add START and END marks to each section
   let indexedSectionsWithMarks =
         map
-          ( \(text :: [String], indx :: Integer) ->
-              ( ["START OF CHAPTER " ++ show indx] ++ text ++ ["END OF CHAPTER " ++ show indx],
-                indx
-              )
+          ( \(text, indx) -> (addMarks text indx, indx)
           )
           indexedSections
-  
+
   -- Return a list of actions to save the chapters to the file system
   map
-    ( \(text :: [String], indx :: Integer) -> do
+    ( \(text, indx) -> do
         writeFile ("chapter_" ++ show indx ++ ".txt") (unlines text)
     )
     indexedSectionsWithMarks
